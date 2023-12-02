dd5c967cfbcf084f90ffe3910b4d98193597d052
Erick-Bravo <erickbrvo@gmail.com>
2023-12-01 16:12:13 -0800

    Navbar styled. Hero layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,41 @@
 import styled from "styled-components";
+import Navbar from "./Navbar";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Hero = () => {
   return (
-   <Section>This is the Hero component</Section>
-  )
-}
+    <Section>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Hello! My name is Erick Bravo</Title>
+          <Header>
+            <Line src="./img/line.png" />
+            <Subtitle>Software Developer</Subtitle>
+          </Header>
+        </Left>
+        <Right>
+            {/*3D model */}
+            <Img src="moon.png" />
+        </Right>
+      </Container>
+    </Section>
+  );
+};
 
 export default Hero;
