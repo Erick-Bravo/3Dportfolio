06be303904fd306fc7af6980a3cf41230313906f
Erick-Bravo <erickbrvo@gmail.com>
2023-12-02 10:40:23 -0800

    hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Navbar from "./Navbar";
 
 const Section = styled.div`
@@ -17,6 +17,68 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const WhatIDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Line = styled.img`
+  height: 5px;
+`;
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgrey;
+`;
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+`;
+
+
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
+`;
+
 const Hero = () => {
   return (
     <Section>
@@ -24,14 +86,15 @@ const Hero = () => {
       <Container>
         <Left>
           <Title>Hello! My name is Erick Bravo</Title>
-          <Header>
+          <WhatIDo>
             <Line src="./img/line.png" />
             <Subtitle>Software Developer</Subtitle>
-          </Header>
+          </WhatIDo>
+          <Button>Learn More</Button>
         </Left>
         <Right>
-            {/*3D model */}
-            <Img src="moon.png" />
+          {/*3D model */}
+          <Img src="./img/moon.png" />
         </Right>
       </Container>
     </Section>
