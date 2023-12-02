2cba069f2f35cade163b9a0d642dabe9b5e47385
Erick-Bravo <erickbrvo@gmail.com>
2023-12-02 11:08:19 -0800

    works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,12 +3,63 @@ import styled from "styled-components";
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
 `;
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const ListItem = styled.li`
+  font-size: 100px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+`;
+
+const data = [
+  "Developer",
+  "Collaborator",
+  "Innovator",
+  "Team Player",
+  "Job Ready",
+];
 
 const Works = () => {
   return (
-   <Section>This is the Works component</Section>
-  )
-}
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem>{item}</ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
+};
 
 export default Works;
