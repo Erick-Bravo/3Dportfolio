0b995b76d7954ea32191f5da3c0faf9bc853251b
Erick-Bravo <erickbrvo@gmail.com>
2023-12-03 12:12:30 -0800

    cube with text spining

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,9 @@
 import {
   OrbitControls,
-  PerspectiveCamera,
-  RenderTexture,
-  Text,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
+import Cube from "./Cube";
 
 const Container = styled.div`
   height: 100vh;
@@ -14,24 +12,14 @@ const Container = styled.div`
 `;
 
 const Test = () => {
+  
   return (
     <Container>
       <Canvas>
         <OrbitControls enableZoom={false} autoRotate={true} />
-        <ambientLight intensity={1} />
+        <ambientLight intensity={2} />
         <directionalLight position={[3, 2, 1]} />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial>
-            <RenderTexture attach="map">
-              <PerspectiveCamera makeDefault position={[0, 0, 2]} />
-              <color attach="background" args={["pink"]} />
-              <Text fontSize={1} color="#555">
-                Hello
-              </Text>
-            </RenderTexture>
-          </meshStandardMaterial>
-        </mesh>
+        <Cube />
       </Canvas>
     </Container>
   );
