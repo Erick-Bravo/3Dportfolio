ffd6c8c4d6322d1041a71a6ad7dd8386f1668edc
Erick-Bravo <erickbrvo@gmail.com>
2023-12-05 23:33:56 -0800

    mobile contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,6 +19,9 @@ const Left = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  @media only screen and (max-width: 768px) {
+    justify-content: center;
+  }
 `;
 const Title = styled.h1`
   font-weight: 200;
@@ -29,6 +32,9 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 25px;
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+  }
 `;
 const Input = styled.input`
   padding: 20px;
@@ -44,6 +50,10 @@ const TextArea = styled.textarea`
 `;
 const Right = styled.div`
   flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 const Button = styled.button`
   background-color: #da4ea2;
